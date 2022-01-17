5fa13d57dd2bfd245e52d546993ce9a54c6544ad
kananykhin_do <eyehategud@gmail.com>
2022-01-17 20:07:43 +0700

    небольшое исправление форматирования

--- src/test/java/LabelsTests.java
+++ src/test/java/LabelsTests.java
@@ -1,24 +1,16 @@
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
-import static org.openqa.selenium.By.partialLinkText;
 
 public class LabelsTests {
 
     @Test
     public void lambdaLabelsTests() {
-    Allure.feature("Issues");
-    Allure.story("Создание Issue для авторизованного пользователя");
-    Allure.label("severity", SeverityLevel.BLOCKER.value());
-    Allure.label("owner", "antibueno");
-    Allure.link("Github", "https://github.com");
+        Allure.feature("Issues");
+        Allure.story("Создание Issue для авторизованного пользователя");
+        Allure.label("severity", SeverityLevel.BLOCKER.value());
+        Allure.label("owner", "antibueno");
+        Allure.link("Github", "https://github.com");
     }
 
     @Test
